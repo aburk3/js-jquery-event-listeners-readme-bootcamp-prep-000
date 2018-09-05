fea5c9ca0dd2579ea4581c9a683b543b3453d323
Austin  Burke <aburk3@gmail.com>
2018-09-05 10:22:53 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -21,7 +21,7 @@ function pressIt() {
 }
 
 function submitIt() {
-  $("form").on("submit", function() {
+  $("form").on("submit", function(e) {
     if ($("input:first").val() === "correct") {
       alert('your form is going to be submitted now')
       return
